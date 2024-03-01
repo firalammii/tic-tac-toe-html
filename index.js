638a9cc82c5b041dc84028b83831212857e5aa57
firalammii <90164296+firalammii@users.noreply.github.com>
2024-03-01 14:29:05 +0300

    simplified

--- index.js
+++ index.js
@@ -1,76 +1,68 @@
 
 const cells = document.querySelectorAll('.cell');
+let c = new Array(9).fill('');
+
 const cell = document.querySelector('.cell');
 const playerName = document.querySelector('.cell');
-
-let c1 = document.getElementById('1').textContent;
-let c2 = document.getElementById('2').textContent;
-let c3 = document.getElementById('3').textContent;
-let c4 = document.getElementById('4').textContent;
-let c5 = document.getElementById('5').textContent;
-let c6 = document.getElementById('6').textContent;
-let c7 = document.getElementById('7').textContent;
-let c8 = document.getElementById('8').textContent;
-let c9 = document.getElementById('9').textContent;
-
 
 let xWins = parseInt(document.getElementById('xwins').textContent);
 let xScore = parseInt(document.getElementById('xscore').textContent);
 let oWins = parseInt(document.getElementById('owins').textContent);
 let oScore = parseInt(document.getElementById('oscore').textContent);
 
-
-
-
-
-let currentPlayerKey = 'X';
+let currentPlayerKey = '';
 let moveCount = 0;
 let winner = false;
 let earlierWin = '';
 
-markCurrentPlayer();
+getNewCellsContent();
+updatePlayerTurn();
 
 cells.forEach(cell => {
 	if (cell.textContent !== '')
 		cell.classList.add('cursor-disabled');
 });
 
+function endCelebration () {
+	document.querySelector('.celebrator').classList.add('hide');
+}
+function startCelebration () {
+	document.querySelector('.celebrator').classList.remove('hide');
+}
+function updateGameStatus (status) {
+	document.getElementById('gamestatus').textContent = status;
+}
+
 function marker (id) {
 	const currCell = document.getElementById(id);
 	if (!winner && !currCell.textContent) {
 		currCell.textContent = currentPlayerKey;
 		moveCount++;
-
-		if (isWon()) {
+		if (moveCount >= 5 && isWon()) {
 			winner = true;
-			document.querySelector('.winner').classList.toggle('hide');
+			startCelebration();
 			document.getElementById('winner-key').textContent = currentPlayerKey;
-			// console.log(`player ${currentPlayerKey} ${isWon()}`);
 			currentPlayerKey == 'X' ? xWins++ : oWins++;
 			if (currentPlayerKey == 'X') {
-				if (earlierWin == 'X')
-					xScore += 10;
-				else
-					xScore += 5;
+				if (earlierWin == 'X') xScore += 10;
+				else xScore += 5;
 			} else {
-				if (earlierWin == 'O')
-					oScore += 10;
-				else
-					oScore += 5;
+				if (earlierWin == 'O') oScore += 10;
+				else oScore += 5;
 			}
 			updateScores();
-			document.getElementById('gamestatus').textContent = 'Win';
+			updateGameStatus('Win');
 			earlierWin = currentPlayerKey;
-		} else if (c1 && c2 && c3 && c4 && c5 && c6 && c7 && c8 && c9)
-			restart();
-		else {
-			currentPlayerKey = currentPlayerKey === 'X' ? 'O' : 'X';
-			document.getElementById('gamestatus').textContent = 'On Going';
+		} else if (c[0] && c[1] && c[2] && c[3] && c[4] && c[5] && c[6] && c[7] && c[8]) {
+			updateGameStatus('Draw');
+		} else {
+			updateGameStatus('On Going');
 		}
-		markCurrentPlayer();
+		updatePlayerTurn();
 	}
 }
-function markCurrentPlayer () {
+function updatePlayerTurn () {
+	currentPlayerKey = currentPlayerKey === 'X' ? 'O' : 'X';
 	document.getElementById('turn').textContent = currentPlayerKey;
 	document.getElementById('key').textContent = currentPlayerKey;
 }
@@ -85,9 +77,10 @@ function newGame () {
 	oWins = 0;
 	oScore = 0;
 	earlierWin = '';
-	document.querySelector('.winner').classList.add('hide');
-	document.getElementById('gamestatus').textContent = 'New Game';
+	endCelebration();
+	updateGameStatus('New Game');
 	updateScores();
+
 }
 function updateScores () {
 	document.getElementById('xscore').textContent = xScore;
@@ -97,34 +90,23 @@ function updateScores () {
 }
 
 function restart () {
-	// draw ?
 	cells.forEach(cell => cell.textContent = '');
 	moveCount = 0;
-
 	earlierWin = currentPlayerKey;
-	currentPlayerKey = currentPlayerKey === 'X' ? 'O' : 'X';
-	markCurrentPlayer();
 	winner = false;
-	document.querySelector('.winner').classList.add('hide');
-	document.getElementById('gamestatus').textContent = 'Game Restarted';
+	updateGameStatus('Game Restarted');
+	endCelebration();
+	updatePlayerTurn();
+}
+function getNewCellsContent () {
+	cells.forEach((cell, index) => c[index] = cell.textContent);
+}
 
-}
-function getCellsUpdate () {
-	c1 = document.getElementById('1').textContent;
-	c2 = document.getElementById('2').textContent;
-	c3 = document.getElementById('3').textContent;
-	c4 = document.getElementById('4').textContent;
-	c5 = document.getElementById('5').textContent;
-	c6 = document.getElementById('6').textContent;
-	c7 = document.getElementById('7').textContent;
-	c8 = document.getElementById('8').textContent;
-	c9 = document.getElementById('9').textContent;
-}
 function isWon () {
-	getCellsUpdate();
-	return c1 !== '' && (c1 == c2 && c2 == c3 || c1 == c4 && c4 == c7 || c1 == c5 && c5 == c9) ||
-		c2 != '' && c2 == c5 && c5 == c8 ||
-		c3 != '' && (c3 == c5 && c5 == c7 || c3 == c6 && c6 == c9) ||
-		c4 != '' && c4 == c5 && c5 == c6 ||
-		c7 != '' && c7 == c8 && c8 == c9;
+	getNewCellsContent();
+	return c[0] && (c[0] == c[1] && c[1] == c[2] || c[0] == c[3] && c[3] == c[6] || c[0] == c[4] && c[4] == c[8]) ||
+		c[1] && c[1] == c[4] && c[4] == c[7] ||
+		c[2] && (c[2] == c[4] && c[4] == c[6] || c[2] == c[5] && c[5] == c[8]) ||
+		c[3] && c[3] == c[4] && c[4] == c[5] ||
+		c[6] && c[6] == c[7] && c[7] == c[8];
 }
